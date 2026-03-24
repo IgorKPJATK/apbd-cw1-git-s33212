c859a9fd647504b27f79cb463a7f6b7c7724871a
Igor Kozuchowski <s33212@pjwstk.edu.pl>
2026-03-24 21:56:17 +0100

    add subtract func

--- apbd-cw1-git-s33212/Program.cs
+++ apbd-cw1-git-s33212/Program.cs
@@ -3,11 +3,17 @@
     static void Main(string[] args)
     {
         Console.WriteLine("--- Kalkulator ---");
-        Console.Write("Dodawanie 6 i 7: " + Add(7,6));
+        Console.Write("Dodawanie 6 i 7: " + Add(7,6) + '\n');
+        Console.Write("Odejmowanie 6 i 7: " + Subtract(6,7) + '\n');
     }
     
     static int Add(int a, int b)
     {
         return a + b;
+    }
+    
+    static int Subtract(int a, int b)
+    {
+        return a - b;
     }
 }
