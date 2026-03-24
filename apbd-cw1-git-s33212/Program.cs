acde45641c6be9a6b44a7dcc728b991788577b68
Igor Kozuchowski <s33212@pjwstk.edu.pl>
2026-03-24 21:53:47 +0100

    add add func

--- apbd-cw1-git-s33212/Program.cs
+++ apbd-cw1-git-s33212/Program.cs
@@ -3,5 +3,11 @@
     static void Main(string[] args)
     {
         Console.WriteLine("--- Kalkulator ---");
+        Console.Write("Dodawanie 6 i 7: " + Add(7,6));
+    }
+    
+    static int Add(int a, int b)
+    {
+        return a + b;
     }
 }
